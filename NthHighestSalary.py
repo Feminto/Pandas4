479dd670132860ae9a8da1108caca2642d10f902
Feminto <feminto@gmail.com>
2025-06-12 10:45:26 -0700

    Adding method 2 for solving Nth Highest Salary problem

--- NthHighestSalary.py
+++ NthHighestSalary.py
@@ -9,3 +9,27 @@ def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
 
     nth_sal = unq_sal[N-1] # Nth highest will be calculated as N-1 since the Indexing starts at 0. Eg. 2nd hishest will be at Index 1
     return pd.DataFrame({f'getNthHighestSalary({N})':[nth_sal]})
+
+
+# Method 2
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    r_set = set()
+    for i in range(len(employee)):
+        sal = employee['salary'][i]
+        r_set.add(sal)
+
+    # print(r_set)
+    result = []
+    for i in r_set:
+        result.append(i)
+
+    unq_sal = sorted(result, reverse = True)
+
+    #invalid cases
+    if len(unq_sal) < N or N <= 0:
+        return pd.DataFrame({f'getNthHighestSalary({N})':[None]})
+
+    nth_sal = unq_sal[N-1] # Nth highest will be calculated as N-1 since the Indexing starts at 0. Eg. 2nd hishest will be at Index 1
+    return pd.DataFrame({f'getNthHighestSalary({N})':[nth_sal]})
