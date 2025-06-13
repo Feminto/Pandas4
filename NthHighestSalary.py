7e10e8c2642892da513739a257c26d120b4b514e
Feminto <feminto@gmail.com>
2025-06-12 19:35:50 -0700

    Adding comments within the code of Nth Highest Salary solution

--- NthHighestSalary.py
+++ NthHighestSalary.py
@@ -1,3 +1,4 @@
+#Method 1
 import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
@@ -6,11 +7,16 @@ def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
     #invalid cases
     if len(unq_sal) < N or N <= 0:
         return pd.DataFrame({f'getNthHighestSalary({N})':[None]})
-
-    nth_sal = unq_sal[N-1] # Nth highest will be calculated as N-1 since the Indexing starts at 0. Eg. 2nd hishest will be at Index 1
+    else:
+        nth_sal = unq_sal[N-1] # Nth highest will be calculated as N-1 since the Indexing starts at 0. Eg. 2nd hishest will be at Index 1
+    
+    # if invalid case is evaluated later, it causes Runtime Error:
+    # nth_sal will try to access an index that might not exist in unq_sal. 
+    # If N is invalid (e.g., N > len(unq_sal) or N <= 0), then: unq_sal[N-1] throws an IndexError.
+    
     return pd.DataFrame({f'getNthHighestSalary({N})':[nth_sal]})
 
-
+   
 # Method 2
 import pandas as pd
 
